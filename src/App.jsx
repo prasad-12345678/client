8341976e5a473f9c2e8637452d927cbca30ede62
prasad-12345678 <167602938+prasad-12345678@users.noreply.github.com>
2026-06-26 19:01:17 +0530

    new version

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import React, { useRef } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import {useUser, useAuth} from '@clerk/clerk-react'
+import Layout from './pages/Layout'
+import toast, {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchUser } from './features/user/userSlice'
+import { fetchConnections } from './features/connections/connectionsSlice'
+import { addMessage } from './features/messages/messagesSlice'
+import Notification from './components/Notification'
+import socket from "./socket";
+
+const App = () => {
+  const {user} = useUser()
+  const {getToken } = useAuth()
+  const {pathname} = useLocation()
+  const pathnameRef = useRef(pathname)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      if(user){
+      const token = await getToken()
+      dispatch(fetchUser(token))
+      dispatch(fetchConnections(token))
+      }
+    }
+    fetchData()
+    
+  },[user, getToken, dispatch])
+
+  useEffect(() => {
+  console.log("USE EFFECT RUN");
+
+  if (!user) {
+    console.log("User is null");
+    return;
+  }
+
+  console.log("Registering user:", user.id);
+  socket.emit("register", user.id);
+
+  socket.off("newMessage");
+
+  socket.on("newMessage", (message) => {
+    console.log("MESSAGE RECEIVED:", message);
+
+    const senderId =
+      typeof message.from_user_id === "object"
+        ? message.from_user_id._id
+        : message.from_user_id;
+
+    if (pathnameRef.current === "/messages/" + senderId) {
+      dispatch(addMessage(message));
+    } else {
+      toast.custom(
+        (t) => <Notification t={t} message={message} />,
+        { position: "bottom-right" }
+      );
+    }
+  });
+
+  return () => {
+    socket.off("newMessage");
+  };
+}, [user, dispatch]);
+  
+  return (
+    <>
+      <Toaster />
+      <Routes>
+        <Route path='/' element={ !user ? <Login /> : <Layout/>}>
+          <Route index element={<Feed/>}/>
+          <Route path='messages' element={<Messages/>}/>
+          <Route path='messages/:userId' element={<ChatBox/>}/>
+          <Route path='connections' element={<Connections/>}/>
+          <Route path='discover' element={<Discover/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          <Route path='profile/:profileId' element={<Profile/>}/>
+          <Route path='create-post' element={<CreatePost/>}/>
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/ChatBox.jsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { dummyMessagesData, dummyUserData } from '../assets/assets'
+import { ImageIcon, SendHorizonal } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { useAuth } from '@clerk/clerk-react'
+import api from '../api/axios'
+import { addMessage, fetchMessages, resetMessages } from '../features/messages/messagesSlice'
+import toast from 'react-hot-toast'
+import socket from '../socket'
+
+const ChatBox = () => {
+
+  const {messages} = useSelector((state)=>state.messages)
+  const { userId } = useParams()
+  const { getToken, userId: currentUserId } = useAuth()
+  const dispatch = useDispatch()
+
+  const [text, setText] = useState('')
+  const [image, setImage] = useState(null)
+  const [user, setUser] = useState(null)
+  const messagesEndRef = useRef(null)
+
+  const connections = useSelector((state) => state.connections.connections)
+
+  /** useEffect(() => {
+  const printToken = async () => {
+    const token = await getToken();
+    console.log(token);
+  }; 
+
+  printToken();
+}, []); */
+
+  const fetchUserMessages = async () => {
+    try {
+      const token = await getToken()
+      dispatch(fetchMessages({token, userId}))
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const sendMessage = async () => {
+    try {
+      if(!text && !image) return
+
+      const token = await getToken()
+      const formData = new FormData();
+      formData.append('to_user_id', userId)
+      formData.append('text', text);
+      image && formData.append('image', image);
+
+      const { data } = await api.post('/api/message/send', formData, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      if (data.success) {
+        setText('')
+        setImage(null)
+        dispatch(addMessage(data.message))
+      }else{
+        throw new Error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+
+  useEffect(()=>{
+    fetchUserMessages()
+
+    return ()=>{
+      dispatch(resetMessages())
+    }
+  },[userId])
+
+  useEffect(() => {
+  if (!currentUserId) return;
+
+
+
+  socket.off("newMessage");
+
+  socket.on("newMessage", (message) => {
+    dispatch(addMessage(message));
+  });
+
+  return () => socket.off("newMessage");
+}, [currentUserId, dispatch]);
+
+  useEffect(()=>{
+    if(connections.length > 0){
+      const user = connections.find(connection => connection._id === userId)
+      setUser(user)
+    }
+  },[connections, userId])
+
+  useEffect(()=>{
+    messagesEndRef.current?.scrollIntoView({behavior: "smooth" })
+  },[messages])
+
+  return user && (
+    <div className='flex flex-col h-screen'>
+      <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
+        <img src={user.profile_picture} alt="" className="size-8 rounded-full"/>
+        <div>
+          <p className="font-medium">{user.full_name}</p>
+          <p className="text-sm text-gray-500 -mt-1.5">@{user.username}</p>
+        </div>
+      </div>
+      <div className='p-5 md:px-10 h-full overflow-y-scroll'>
+        <div className='space-y-4 max-w-4xl mx-auto'>
+          {
+           [...messages].sort(
+  (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+).map((message, index)=> {
+
+      const senderId =
+        typeof message.from_user_id === "object"
+          ? message.from_user_id._id
+          : message.from_user_id;
+              return(
+              <div key={index} className={`flex flex-col ${senderId === currentUserId ? 'items-end' : 'items-start'}`}>
+                <div className={`p-2 text-sm max-w-sm bg-white text-slate-700 rounded-lg shadow ${senderId === currentUserId
+  ? 'rounded-br-none'
+  : 'rounded-bl-none'}`}>
+                  {
+                  message.message_type === 'image' && <img src={message.media_url} className='w-full max-w-sm rounded-lg mb-1' alt="" />
+                  }
+                  <p>{message.text}</p>
+                </div>
+
+              </div>
+            ) })
+          }
+          <div ref={messagesEndRef} />
+        </div>
+      </div>
+      <div className='px-4'>
+          <div className='flex items-center gap-3 pl-5 p-1.5 bg-white w-full max-w-xl mx-auto border border-gray-200 shadow rounded-full mb-5'>
+            <input type="text" className='flex-1 outline-none text-slate-700' placeholder='Type a message...'
+            onKeyDown={e=>e.key === 'Enter' && sendMessage()} onChange={(e)=>setText(e.target.value)} value={text} />
+
+            <label htmlFor="image">
+              {
+                image 
+                ? <img src={URL.createObjectURL(image)} alt="" className='h-8 rounded'/> 
+                : <ImageIcon className='size-7 text-gray-400 cursor-pointer'/>
+              }
+              <input type="file" id='image' accept="image/*" hidden onChange={(e)=>setImage(e.target.files[0])}/>
+            </label>
+
+            <button onClick={sendMessage} className='bg-gradient-to-br from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 cursor-pointer text-white p-2 rounded-full'>
+              <SendHorizonal size={18}/>
+            </button>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatBox
